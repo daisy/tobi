5e013605fba007201f30bc5213cc85d80cb73989
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2008-01-20 21:11:09 +0000

    - Daniel WECK * Removed useless test file. * Added backup for Trac CSS style and header customization (because they are not saved anywhere else, so the SVN is a safe place to make a backup)

---
+++ trac/site_header.cs
@@ -0,0 +1,10 @@
+<?cs
+####################################################################
+# Site header - Contents are automatically inserted above Trac HTML
+?>
+
+<div style="float:right;font-size:0.7em;">
+<a href="#content" title="Skip global navigation and jump straight to main content, or local navigation, if any.">Skip Navigation</a>
+ | 
+<a href="http://daisy-trac.cvsdude.com/tobi/wiki/AccessKeys#content" title="Link to the accessibility guide.">Accessibility</a>
+</div>
