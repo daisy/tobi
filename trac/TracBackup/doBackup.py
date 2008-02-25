0197d9f83349a797185fca864efd08361af5be7b
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2008-02-25 12:04:45 +0000

    - Added a backup system (written in Python) for the Trac Wiki pages (excluding the default ones, in other words: only the project-related content).
    - Added the Wiki pages in their current state.
    
    This is an additional measure to the backups made daily by CVS-Dude.com
    Because it is a manual procedure, it won't record all the changes in the Wiki, but at least offers some kind of redundancy, and it's free ! :) (unlike the backup-download service of CVS-Dude).
    
    From now-on, we can launch the Python script regularly (as soon as there are significant changes in the Wiki), in order to let SVN register the changes.

---
+++ trac/TracBackup/doBackup.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from BeautifulSoup import BeautifulSoup
+import urllib2
+from xml.dom import minidom
+from urlparse import urlparse
+import socket
+import os
+
+timeout = 30
+socket.setdefaulttimeout(timeout)
+
+# urlparse ==> <scheme>://<netloc>/<path>;<params>?<query>#<fragment>
+
+rooturl = 'http://daisy-trac.cvsdude.com'
+wikiroot = '/tobi/wiki/'
+baseurl = rooturl + wikiroot + 'TitleIndex'
+
+FILENAME_SLASH_REPLACE = '%SLASH%'
+outputdir = './Wiki/'
+
+tracwikiwords = []
+
+def gethtml(url):
+	print 'Getting URL: [' + url + ']...'
+	html = urllib2.urlopen(url).read()
+	return html
+
+def loaddefaulttracpages():
+	global tracwikiwords
+	filename = 'defaulttracpages.xml'
+	print 'Loading file: [' + filename + ']...'
+	dom = minidom.parse(filename)
+	for node in dom.getElementsByTagName('tracwikiword'):
+		tracwikiwords.append(node.attributes['name'].value)
+	print 'Loaded [' + `len(tracwikiwords)` + '] Trac default wiki pages.'
+
+def extractlinks(html):
+	global tracwikiwords,baseurl,rooturl,wikiroot
+	print 'Parsing the HTML, searching for links...'
+	soup = BeautifulSoup(html)
+	anchors = soup.findAll('a')
+	print 'Found [' + `len(anchors)` + '] <a href="..."> links.'
+	links = []
+	for a in anchors:
+		if a.has_key('href'):
+			str = a['href']
+			if str.startswith(wikiroot):
+				urlstr = rooturl + str
+				print 'Parsing URL: [' + urlstr + ']...'
+				o = urlparse(urlstr)
+				if len([s for s in tracwikiwords if s in o.path]) == 0 and len([s for s in links if o.path in s]) == 0:
+					url = rooturl + str + '?format=txt'
+					wikiword = str[len(wikiroot):]
+					print 'Adding link to download: [' + wikiword + ' %% ' + url + '].'
+					links.append({'wikiword':wikiword,'url':url})
+	print 'Extracted [' + `len(links)` + '] relevant links (excluding Trac\'s default pages).'
+	return links
+
+def downloadpages(links):
+	global FILENAME_SLASH_REPLACE,outputdir
+	print 'Downloading [' + `len(links)` + '] Trac wiki pages...'
+	#not os.access(outputdir, os.F_OK):
+	if not os.path.isdir(outputdir):
+		os.mkdir(outputdir)
+	ext = '.tracwiki.txt'
+	for link in links:
+		filename = outputdir + link['wikiword'].replace('/',FILENAME_SLASH_REPLACE) + ext
+		print 'Creating file [' + filename + ']...'
+		fp = open(filename,"w")
+		url = link['url']
+		print 'Reading URL [' + url + ']...'
+		source = urllib2.urlopen(url).read()
+		print 'Writing file [' + filename + ']...'
+		fp.write(source)
+		fp.close()
+
+def main():
+	global tracwikiwords,baseurl
+	loaddefaulttracpages()
+	html = gethtml(baseurl)
+	links = extractlinks(html)
+	downloadpages(links)
+
+if __name__ == '__main__':
+	main()
