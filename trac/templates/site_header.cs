ff99fc4e2057462416708dc8092bd6a98241ae15
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2008-02-28 00:21:28 +0000

    re-organizing a little

---
+++ trac/templates/site_header.cs
@@ -0,0 +1,32 @@
+<?cs if:title == "guidelines/SourceCode" ?>
+
+<script type="text/javascript" src="/tobi/chrome/uploads/daniel-weck/shCore.js"></script>
+<script type="text/javascript" src="/tobi/chrome/uploads/daniel-weck/shBrushCSharp.js"></script>
+<script type="text/javascript" src="/tobi/chrome/uploads/daniel-weck/shBrushXml.js"></script>
+<script type="text/javascript" src="/tobi/chrome/uploads/daniel-weck/shBrushJava.js"></script>
+
+<script type="text/javascript">
+<!--
+dp.SyntaxHighlighter.HighlightAll('code');
+window.onload=function(){dp.SyntaxHighlighter.HighlightAll('code');}
+//-->
+</script>
+
+<?cs /if ?>
+
+<?cs if:title == "calls" ?>
+
+<script type="text/javascript" src="http://download.skype.com/share/skypebuttons/js/skypeCheck.js"></script>
+
+<?cs /if ?>
+
+<?cs
+####################################################################
+# Site header - Contents are automatically inserted above Trac HTML
+?>
+
+<div style="float:right;font-size:0.7em;">
+<a href="#content" title="Skip global navigation and jump straight to main content, or local navigation, if any.">Skip Navigation</a>
+ | 
+<a href="http://daisy-trac.cvsdude.com/tobi/wiki/AccessKeys#content" title="Link to the accessibility guide.">Accessibility</a>
+</div>
