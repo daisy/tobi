3762ff162cd24c1073107cc89d2c4b236b567f01
stephen-hibberd <stephen-hibberd@0210928e-6ab4-4709-a5c3-da57979d938b>
2008-04-08 15:06:29 +0000

    moved tobi demo under toybox

---
+++ csharp/toybox/Tobi/Tobi/TransportToolBar.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Tobi
+{
+    partial class TransportToolBar
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolBar1 = new System.Windows.Forms.ToolBar();
+            this.button1 = new System.Windows.Forms.ToolBarButton();
+            this.button2 = new System.Windows.Forms.ToolBarButton();
+            this.SuspendLayout();
+            // 
+            // toolBar1
+            // 
+            this.toolBar1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.toolBar1.Buttons.AddRange(new System.Windows.Forms.ToolBarButton[] {
+            this.button1,
+            this.button2});
+            this.toolBar1.DropDownArrows = true;
+            this.toolBar1.Location = new System.Drawing.Point(0, 0);
+            this.toolBar1.Name = "toolBar1";
+            this.toolBar1.ShowToolTips = true;
+            this.toolBar1.Size = new System.Drawing.Size(584, 43);
+            this.toolBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.ImageIndex = 0;
+            this.button1.Name = "button1";
+            this.button1.Text = "button1";
+            // 
+            // button2
+            // 
+            this.button2.Name = "button2";
+            this.button2.Text = "button2";
+            // 
+            // TransportToolBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.toolBar1);
+            this.Name = "TransportToolBar";
+            this.Size = new System.Drawing.Size(584, 35);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolBar toolBar1;
+        private System.Windows.Forms.ToolBarButton button1;
+        private System.Windows.Forms.ToolBarButton button2;
+    }
+}
---
+++ csharp/toybox/Tobi/Tobi/TransportToolStrip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tobi
+{
+    public partial class TransportToolStrip : UserControl
+    {
+        public TransportToolStrip()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
