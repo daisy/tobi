e7cb302713b6f041a991f89200b5d1af22b698eb
ole-holst-andersen <ole-holst-andersen@0210928e-6ab4-4709-a5c3-da57979d938b>
2008-05-22 15:18:08 +0000

    * Changed the extraction visitor class hierarchy a bit * Adding support for lists

---
+++ csharp/toybox/XmlEditor/FlowDocumentXmlEditor/FlowDocumentExtraction/Html/HtmlBlockExtractionVisitor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Documents;
+using urakawa.property.xml;
+using urakawa.core;
+using urakawa.property.channel;
+
+namespace FlowDocumentXmlEditor.FlowDocumentExtraction.Html
+{
+    public class HtmlBlockExtractionVisitor : HtmlGenericExtractionVisitor
+    {
+        private List<Block> mExtractedBlocks = new List<Block>();
+        public List<Block> ExtractedBlocks { get { return mExtractedBlocks; } }
+
+        public HtmlBlockExtractionVisitor(Channel textCh) : base(textCh) { }
+
+        protected override bool HandleHtmlElement(XmlProperty xmlProp, TreeNode node, Run nodeRun)
+        {
+            switch (xmlProp.getLocalName())
+            {
+                case "ul":
+                    List uList = new List();
+                    uList.MarkerStyle = System.Windows.TextMarkerStyle.Disc;
+                    HandleListItems(uList.ListItems, node, nodeRun);
+                    ExtractedBlocks.Add(uList);
+                    return false;
+                case "ol":
+                    List oList = new List();
+                    oList.MarkerStyle = System.Windows.TextMarkerStyle.Decimal;
+                    HandleListItems(oList.ListItems, node, nodeRun);
+                    ExtractedBlocks.Add(oList);
+                    return false;
+                case "p":
+                    Paragraph newPara = new Paragraph();
+                    HandleInlines(newPara.Inlines, node, nodeRun);
+                    ExtractedBlocks.Add(newPara);
+                    return false;
+                default:
+                    break;
+            }
+            return true;
+        }
+
+        public static bool ContainsHtmlBlockChild(TreeNode node)
+        {
+            foreach (TreeNode child in node.getListOfChildren())
+            {
+                if (ContainsHtmlBlock(child)) return true;
+            }
+            return false;
+        }
+
+        public static bool ContainsHtmlBlock(TreeNode node)
+        {
+            XmlProperty xmlProp = node.getProperty<XmlProperty>();
+            if (xmlProp != null)
+            {
+                if (xmlProp.getNamespaceUri() == HtmlNamespace)
+                {
+                    switch (xmlProp.getLocalName())
+                    {
+                        case "p":
+                        case "ul":
+                        case "ol":
+                            return true;
+                    }
+                }
+            }
+            return ContainsHtmlBlockChild(node);
+        }
+
+        private void HandleListItems(ListItemCollection destItemColl, TreeNode node, Run nodeRun)
+        {
+            if (nodeRun != null) destItemColl.Add(new ListItem(new Paragraph(nodeRun)));
+            if (node.getChildCount() > 0)
+            {
+                HtmlListItemEntractionVisitor itemVisitor = new HtmlListItemEntractionVisitor(TextChannel);
+                foreach (TreeNode child in node.getListOfChildren())
+                {
+                    child.acceptDepthFirst(itemVisitor);
+                }
+                destItemColl.AddRange(itemVisitor.ExtractedListItems);
+            }
+        }
+
+        protected override void HandleNodeRun(Run nodeRun)
+        {
+            ExtractedBlocks.Add(new Paragraph(nodeRun));
+        }
+    }
+}
---
+++ csharp/toybox/XmlEditor/FlowDocumentXmlEditor/FlowDocumentExtraction/Html/HtmlGenericExtractionVisitor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using urakawa.property.xml;
+using urakawa.core;
+using System.Windows.Documents;
+using urakawa.property.channel;
+
+namespace FlowDocumentXmlEditor.FlowDocumentExtraction.Html
+{
+    public abstract class HtmlGenericExtractionVisitor : GenericExtractionVisitor
+    {
+        public HtmlGenericExtractionVisitor(Channel textCh) : base(textCh) { }
+
+        public static string HtmlNamespace = "http://www.w3.org/1999/xhtml";
+        protected override bool HandleXmlElement(XmlProperty xmlProp, TreeNode node, Run nodeRun)
+        {
+            if (xmlProp.getNamespaceUri()==HtmlNamespace)
+            {
+                return HandleHtmlElement(xmlProp, node, nodeRun);
+            }
+            return true;
+        }
+
+        protected abstract bool HandleHtmlElement(XmlProperty xmlProp, TreeNode node, Run nodeRun);
+
+        protected void HandleInlines(InlineCollection destInlineColl, TreeNode node, Run nodeRun)
+        {
+            if (nodeRun != null) destInlineColl.Add(nodeRun);
+            if (node.getChildCount() > 0)
+            {
+                HtmlInlineExtractionVisitor inlineVisitor = new HtmlInlineExtractionVisitor(TextChannel);
+                foreach (TreeNode child in node.getListOfChildren())
+                {
+                    child.acceptDepthFirst(inlineVisitor);
+                }
+                destInlineColl.AddRange(inlineVisitor.ExtractedInlines);
+            }
+        }
+
+        protected void HandleBlocks(BlockCollection destBlockColl, TreeNode node, Run nodeRun)
+        {
+            if (nodeRun != null) destBlockColl.Add(new Paragraph(nodeRun));
+            if (node.getChildCount() > 0)
+            {
+                HtmlBlockExtractionVisitor blockVisitor = new HtmlBlockExtractionVisitor(TextChannel);
+                foreach (TreeNode child in node.getListOfChildren())
+                {
+                    child.acceptDepthFirst(blockVisitor);
+                }
+                destBlockColl.AddRange(blockVisitor.ExtractedBlocks);
+            }
+        }
+    }
+}
