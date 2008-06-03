cb51cd8fb3ff493ab0d1b3950b12e83a753e3e93
ole-holst-andersen <ole-holst-andersen@0210928e-6ab4-4709-a5c3-da57979d938b>
2008-06-03 15:18:16 +0000

    A bit more tinkering - unfortunately BindableRun does not seem to work with RichTextEdit control

---
+++ trunk/csharp/toybox/XmlEditor/FlowDocumentXmlEditor/BindableRun.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace FlowDocumentXmlEditor
+{
+    /// <summary>
+    /// A subclass of the Run element that exposes a DependencyProperty property 
+    /// to allow data binding.
+    /// </summary>
+    public class BindableRun : Run
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(BindableRun), new PropertyMetadata(new PropertyChangedCallback(BindableRun.OnTextChanged)));
+
+        private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((Run)d).Text = (string)e.NewValue;
+        }
+
+        public new string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
