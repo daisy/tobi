301f06b570124db547eb6f41572e2a9cf280af1c
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2008-07-09 16:18:42 +0000

    apparently missing commit (minor stuff)

--- trac/templates/site_header.cs
+++ trac/templates/site_header.cs
@@ -26,6 +26,10 @@ window.onload=function(){dp.SyntaxHighlighter.HighlightAll('code');}
 ?>
 
 <div style="float:right;font-size:0.7em;">
+<a accesskey="l" href="/tobi/login" title="Authenticate yourself to gain write-credentials">Login</a>
+ / 
+<a href="/tobi/logout" title="Log-off to become a anonymous user with read credentials only">Logout</a>
+ | 
 <a href="#content" title="Skip global navigation and jump straight to main content, or local navigation, if any.">Skip Navigation</a>
  | 
 <a href="http://daisy-trac.cvsdude.com/tobi/wiki/AccessKeys#content" title="Link to the accessibility guide.">Accessibility</a>
