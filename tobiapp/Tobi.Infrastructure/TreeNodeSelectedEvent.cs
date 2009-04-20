c26b2a240654312c4ccfa1eb6885118282558d40
Daniel Weck <625201+danielweck@users.noreply.github.com>
2009-04-20 12:59:05 +0000

    new feature: the waveform now displays sub-audio-chunks that correspond to individual sentences (the finest synchronization granularity). There are some highlight problems at playback when for example a section is selected and its sub-audio-chunks are being played in sequence: some dirty color regions are not updated properly. Otherwise the subdivision of the waveform into individual ranges works fine, including the mouse-click selection in the waveform display.

--- tobiapp/Tobi.Infrastructure/TreeNodeSelectedEvent.cs
+++ tobiapp/Tobi.Infrastructure/TreeNodeSelectedEvent.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Practices.Composite.Presentation.Events;
+﻿using Microsoft.Practices.Composite.Presentation.Events;
 using urakawa.core;
 
 namespace Tobi.Infrastructure
 {
     public class TreeNodeSelectedEvent : CompositePresentationEvent<TreeNode>
+    {
+    }
+    public class SubTreeNodeSelectedEvent : CompositePresentationEvent<TreeNode>
     {
     }
 }
