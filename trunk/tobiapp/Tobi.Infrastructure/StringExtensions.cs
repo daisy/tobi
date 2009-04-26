f6be9b9c2f4b0b257e8c57e6752baf19a210f650
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2009-04-26 00:04:19 +0000

    one more utility class

---
+++ trunk/tobiapp/Tobi.Infrastructure/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Tobi.Infrastructure
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Returns a byte array based
+        /// on the given
+        /// <see cref="System.Text.Encoding"/>.
+        /// </summary>
+        /// <typeparam name="TEncoding">The
+        /// Encoding to use for conveting from the string of characters to bytes (e.g. ASCIIEncoding).</typeparam>
+        /// <param name="source">The string to convert
+        /// to a byte array.</param>
+        /// <returns>The byte array corresponding to the string and based
+        /// on the supplied
+        /// <see cref="System.Text.Encoding"/>
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException" />
+        public static byte[] ToByteArray<TEncoding>(
+            this string source)
+            where TEncoding : System.Text.Encoding, new()
+        {
+            return new TEncoding().GetBytes(source.ToCharArray());
+        }
+    }
+}
