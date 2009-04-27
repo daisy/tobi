8761afae1a5cfb670d1439f6af79e17d0b9a6744
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2009-04-27 13:12:16 +0000

    a TabControl subclass that still lazy-load the TabItem content (i.e. not greedy), but does not unload the contents when switching tabs.

---
+++ trunk/tobiapp/Tobi.Infrastructure/UI/LazyKeepAliveTabControl.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Tobi.Infrastructure.UI
+{
+    [TemplatePart(Name = "PART_ItemsHolder", Type = typeof(Panel))]
+    public class LazyKeepAliveTabControl : TabControl
+    {
+        private Panel m_ItemsHolder;
+
+        public LazyKeepAliveTabControl()
+        {
+            // this is necessary so that we get the initial databound selected item
+            ItemContainerGenerator.StatusChanged += OnItemContainerGeneratorStatusChanged;
+        }
+
+        /// <summary>
+        /// if containers are done, generate the selected item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnItemContainerGeneratorStatusChanged(object sender, EventArgs e)
+        {
+            if (ItemContainerGenerator.Status == GeneratorStatus.ContainersGenerated)
+            {
+                ItemContainerGenerator.StatusChanged -= OnItemContainerGeneratorStatusChanged;
+                UpdateSelectedItem();
+            }
+        }
+
+        /// <summary>
+        /// get the ItemsHolder and generate any children
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            m_ItemsHolder = GetTemplateChild("PART_ItemsHolder") as Panel;
+            UpdateSelectedItem();
+        }
+
+        /// <summary>
+        /// when the items change we remove any generated panel children and add any new ones as necessary
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnItemsChanged(e);
+
+            if (m_ItemsHolder == null)
+            {
+                return;
+            }
+
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Reset:
+                    m_ItemsHolder.Children.Clear();
+                    break;
+
+                case NotifyCollectionChangedAction.Add:
+                case NotifyCollectionChangedAction.Remove:
+                    if (e.OldItems != null)
+                    {
+                        foreach (var item in e.OldItems)
+                        {
+                            ContentPresenter cp = FindChildContentPresenter(item);
+                            if (cp != null)
+                            {
+                                m_ItemsHolder.Children.Remove(cp);
+                            }
+                        }
+                    }
+
+                    // don't do anything with new items because we don't want to
+                    // create visuals that aren't being shown
+
+                    UpdateSelectedItem();
+                    break;
+
+                case NotifyCollectionChangedAction.Replace:
+                    throw new NotImplementedException("Replace not implemented yet");
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// update the visible child in the ItemsHolder
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnSelectionChanged(SelectionChangedEventArgs e)
+        {
+            base.OnSelectionChanged(e);
+            UpdateSelectedItem();
+        }
+
+        /// <summary>
+        /// generate a ContentPresenter for the selected item
+        /// </summary>
+        void UpdateSelectedItem()
+        {
+            if (m_ItemsHolder == null)
+            {
+                return;
+            }
+
+            // generate a ContentPresenter if necessary
+            TabItem item = GetSelectedTabItem();
+            if (item != null)
+            {
+                CreateChildContentPresenter(item);
+            }
+
+            // show the right child
+            foreach (ContentPresenter child in m_ItemsHolder.Children)
+            {
+                if (child.Tag != null && child.Tag is TabItem)
+                {
+                    child.Visibility = (((TabItem)child.Tag).IsSelected)
+                                           ? Visibility.Visible
+                                           : Visibility.Collapsed;
+                }
+            }
+        }
+
+        /// <summary>
+        /// create the child ContentPresenter for the given item (could be data or a TabItem)
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        void CreateChildContentPresenter(object item)
+        {
+            if (item == null)
+            {
+                return;
+            }
+
+            ContentPresenter cp = FindChildContentPresenter(item);
+
+            if (cp != null)
+            {
+                return;
+            }
+
+            // the actual child to be added.  cp.Tag is a reference to the TabItem
+            cp = new ContentPresenter
+                     {
+                         Content = (item is TabItem) ? (item as TabItem).Content : item,
+                         ContentTemplate = SelectedContentTemplate,
+                         ContentTemplateSelector = SelectedContentTemplateSelector,
+                         ContentStringFormat = SelectedContentStringFormat,
+                         Visibility = Visibility.Collapsed,
+                         Tag = (item is TabItem) ? item : (ItemContainerGenerator.ContainerFromItem(item))
+                     };
+            m_ItemsHolder.Children.Add(cp);
+            return;
+        }
+
+        /// <summary>
+        /// Find the CP for the given object.  data could be a TabItem or a piece of data
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        ContentPresenter FindChildContentPresenter(object data)
+        {
+            if (data is TabItem)
+            {
+                data = (data as TabItem).Content;
+            }
+
+            if (data == null)
+            {
+                return null;
+            }
+
+            if (m_ItemsHolder == null)
+            {
+                return null;
+            }
+
+            foreach (ContentPresenter cp in m_ItemsHolder.Children)
+            {
+                if (cp.Content == data)
+                {
+                    return cp;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// copied from TabControl; wish it were protected in that class instead of private
+        /// </summary>
+        /// <returns></returns>
+        protected TabItem GetSelectedTabItem()
+        {
+            object selectedItem = SelectedItem;
+            if (selectedItem == null)
+            {
+                return null;
+            }
+            var item = selectedItem as TabItem ??
+                       ItemContainerGenerator.ContainerFromIndex(SelectedIndex) as TabItem;
+            return item;
+        }
+    }
+}
