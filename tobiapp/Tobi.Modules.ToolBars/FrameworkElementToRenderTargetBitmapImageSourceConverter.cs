c7bc0b9b16efdaf52b28778fb849a7a591922c44
Daniel Weck <625201+danielweck@users.noreply.github.com>
2009-04-28 12:17:37 +0000

    fixed the icon scaling issue

--- tobiapp/Tobi.Modules.ToolBars/FrameworkElementToRenderTargetBitmapImageSourceConverter.cs
+++ tobiapp/Tobi.Modules.ToolBars/FrameworkElementToRenderTargetBitmapImageSourceConverter.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace Tobi.Modules.ToolBars
 {
@@ -29,18 +30,38 @@ namespace Tobi.Modules.ToolBars
                 return null;
             }
 
-            var renderBitmap = new RenderTargetBitmap((int)image.Width, (int)image.Height, 96, 96, PixelFormats.Pbgra32);
+            var visualBrush = new VisualBrush(frameworkElement);
+            var rectangle = new Rectangle { SnapsToDevicePixels = false, StrokeThickness = 0, Height = image.Height, Width = image.Width, Fill = visualBrush };
 
             var size = new Size(image.Width, image.Height);
-            frameworkElement.Measure(size);
-            frameworkElement.Arrange(new Rect(0, 0, image.Width, image.Height));
-            frameworkElement.UpdateLayout();
-            renderBitmap.Render(frameworkElement);
+            rectangle.Measure(size);
+            rectangle.Arrange(new Rect(0, 0, image.Width, image.Height));
+            rectangle.UpdateLayout();
+
+            var renderBitmap = new RenderTargetBitmap((int)image.Width, (int)image.Height, 96, 96, PixelFormats.Pbgra32);
+            renderBitmap.Render(rectangle);
 
             renderBitmap.Freeze();
             return renderBitmap;
 
             //return renderBitmap.GetAsFrozen();
+
+
+            /*
+            var viewBox = new Viewbox
+            {
+                VerticalAlignment = VerticalAlignment.Stretch,
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                StretchDirection = StretchDirection.Both,
+                Stretch = Stretch.Fill
+            };
+            viewBox.Child = frameworkElement;
+             */
+            /*
+            viewBox.Measure(size);
+            viewBox.Arrange(new Rect(0, 0, image.Width, image.Height));
+            viewBox.UpdateLayout();
+             */
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
