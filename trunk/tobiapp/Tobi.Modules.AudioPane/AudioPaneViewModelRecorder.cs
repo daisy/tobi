9ec5aabcbf003df2997110dbad0154eb98b2ee30
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2009-05-03 19:43:12 +0000

    enabled basic recording, but had problems with VMWare Fusion because the virtual SoundBlaster PCI was not the default input device.

--- trunk/tobiapp/Tobi.Modules.AudioPane/AudioPaneViewModelRecorder.cs
+++ trunk/tobiapp/Tobi.Modules.AudioPane/AudioPaneViewModelRecorder.cs
@@ -1,5 +1,8 @@
-﻿using AudioLib;
+﻿using System.IO;
+using AudioLib;
 using Microsoft.Practices.Composite.Logging;
+using urakawa.media.data.audio;
+using urakawa.media.data.audio.codec;
 
 namespace Tobi.Modules.AudioPane
 {
@@ -15,6 +18,36 @@ namespace Tobi.Modules.AudioPane
         // ReSharper restore MemberCanBeMadeStatic.Local
         {
             //m_Recorder.State == AudioLib.AudioRecorderState.Monitoring
+            Logger.Log("AudioPaneViewModel.OnRecorderStateChanged", Category.Debug, Priority.Medium);
+
+            if ((e.OldState == AudioRecorderState.Recording || e.OldState == AudioRecorderState.Monitoring)
+                && m_Recorder.State == AudioRecorderState.Stopped)
+            {
+                UpdatePeakMeter();
+                if (View != null)
+                {
+                    View.StopPeakMeterTimer();
+                }
+            }
+            if (m_Recorder.State == AudioRecorderState.Recording || m_Recorder.State == AudioRecorderState.Monitoring)
+            {
+                if (e.OldState == AudioRecorderState.Stopped)
+                {
+                    PeakOverloadCountCh1 = 0;
+                    PeakOverloadCountCh2 = 0;
+                }
+                UpdatePeakMeter();
+                if (View != null)
+                {
+                    View.StartPeakMeterTimer();
+                }
+            }
+        }
+
+        public bool IsRecording
+        {
+            get;
+            set;
         }
 
         public void AudioRecorder_StartStop()
@@ -29,9 +62,28 @@ namespace Tobi.Modules.AudioPane
                 AudioRecorder_Start();
             }
         }
+
         public void AudioRecorder_Start()
         {
             Logger.Log("AudioPaneViewModel.AudioRecorder_Start", Category.Debug, Priority.Medium);
+
+            var shell = Container.Resolve<IShellPresenter>();
+
+            if (shell.DocumentProject == null)
+            {
+                setRecordingDirectory(Directory.GetCurrentDirectory());
+                m_PcmFormat = new PCMFormatInfo();
+            }
+            else
+            {
+                if (m_PcmFormat == null)
+                {
+                    m_PcmFormat = shell.DocumentProject.GetPresentation(0).MediaDataManager.DefaultPCMFormat;
+                }
+            }
+
+            m_Recorder.StartRecording(m_PcmFormat);
+
             IsRecording = true;
             OnPropertyChanged("IsRecording");
         }
@@ -39,8 +91,28 @@ namespace Tobi.Modules.AudioPane
         public void AudioRecorder_Stop()
         {
             Logger.Log("AudioPaneViewModel.AudioRecorder_Stop", Category.Debug, Priority.Medium);
+
+            var shell = Container.Resolve<IShellPresenter>();
+
+            if (shell.DocumentProject != null)
+            {
+                var mediaData =
+                    (WavAudioMediaData) shell.DocumentProject.GetPresentation(0).MediaDataFactory.CreateAudioMediaData();
+
+                ManagedAudioMedia managedMedia =
+                    shell.DocumentProject.GetPresentation(0).MediaFactory.CreateManagedAudioMedia();
+                managedMedia.MediaData = mediaData;
+            }
+
+            m_Recorder.StopRecording();
+
             IsRecording = false;
             OnPropertyChanged("IsRecording");
+
+            if (!string.IsNullOrEmpty(m_Recorder.RecordedFilePath))
+            {
+                OpenFile(m_Recorder.RecordedFilePath);
+            }
         }
 
         #endregion Audio Recorder
