e08821ad9b7f4610046f8b3f64bbf1f429cb9ddd
Daniel Weck <625201+danielweck@users.noreply.github.com>
2009-05-07 22:05:21 +0000

    oops, I broke the compile in my previous commit. Also added new attached commands triggered by property change, events, or a group of triggers.

---
+++ tobiapp/Tobi.Infrastructure/Commanding/AttachedTrigger/CommandTrigger.cs
@@ -0,0 +1,95 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+// See http://blogs.microsoft.co.il/blogs/tomershamam/archive/2009/04/14/wpf-commands-everywhere.aspx
+
+namespace Tobi.Infrastructure.Commanding.AttachedTrigger
+{
+    public abstract class CommandTrigger : Freezable, ICommandTrigger
+    {
+        public bool IsInitialized { get; set; }
+
+        #region Dependency Properties
+
+        #region Command Property
+
+        /// <value>Identifies the Command dependency property</value>
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(CommandTrigger),
+                                        new FrameworkPropertyMetadata(null));
+
+        /// <value>description for Command property</value>
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        #endregion
+
+        #region CustomParameter Property
+
+        /// <value>Identifies the CustomParameterProperty dependency property</value>
+        public static readonly DependencyProperty CustomParameterProperty =
+            DependencyProperty.Register("CustomParameter", typeof(object), typeof(CommandTrigger),
+                                        new FrameworkPropertyMetadata(null));
+
+        /// <value>description for CustomParameter property</value>
+        public object CustomParameter
+        {
+            get { return (object)GetValue(CustomParameterProperty); }
+            set { SetValue(CustomParameterProperty, value); }
+        }		
+
+        #endregion
+
+        #region CommandTarget Property
+
+        /// <value>Identifies the CommandTarget dependency property</value>
+        public static readonly DependencyProperty CommandTargetProperty =
+            DependencyProperty.Register("CommandTarget", typeof(IInputElement), typeof(CommandTrigger),
+                                        new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.Inherits));
+
+        /// <value>description for CommandTarget property</value>
+        public IInputElement CommandTarget
+        {
+            get { return (IInputElement)GetValue(CommandTargetProperty); }
+            set { SetValue(CommandTargetProperty, value); }
+        }
+
+        #endregion
+		
+        #endregion
+
+        #region Internals
+
+        void ICommandTrigger.Initialize(FrameworkElement source)
+        {
+            if (IsInitialized)
+                return;
+			
+            InitializeCore(source);
+            IsInitialized = true;
+        }
+
+        protected abstract void InitializeCore(FrameworkElement source);
+
+        protected void ExecuteCommand(CommandParameter<object> parameter)
+        {
+            if (Command == null)
+                return;
+
+            RoutedCommand routedCommand = Command as RoutedCommand;
+            if (routedCommand != null)
+            {
+                routedCommand.Execute(parameter, CommandTarget);
+            }
+            else
+            {
+                Command.Execute(parameter);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ tobiapp/Tobi.Infrastructure/Commanding/AttachedTrigger/EventCommandTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+
+// See http://blogs.microsoft.co.il/blogs/tomershamam/archive/2009/04/14/wpf-commands-everywhere.aspx
+
+namespace Tobi.Infrastructure.Commanding.AttachedTrigger
+{
+    public sealed class EventCommandTrigger : CommandTrigger
+    {
+        #region Dependency Properties
+
+        #region RoutedEvent Property
+
+        /// <value>Identifies the RoutedEvent dependency property</value>
+        public static readonly DependencyProperty RoutedEventProperty =
+            DependencyProperty.Register("RoutedEvent", typeof(RoutedEvent), typeof(EventCommandTrigger),
+                                        new FrameworkPropertyMetadata(null));
+
+        /// <value>description for RoutedEvent property</value>
+        public RoutedEvent RoutedEvent
+        {
+            get { return (RoutedEvent)GetValue(RoutedEventProperty); }
+            set { SetValue(RoutedEventProperty, value); }
+        }
+
+        #endregion
+		
+        #endregion
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new EventCommandTrigger();
+        }
+
+        protected override void InitializeCore(FrameworkElement source)
+        {
+            source.AddHandler(RoutedEvent, (RoutedEventHandler)ExecuteCommand);
+        }
+
+        private void ExecuteCommand(object sender, RoutedEventArgs args)
+        {
+            CommandParameter<object> parameter = new EventCommandParameter<object, RoutedEventArgs>(
+                CustomParameter, RoutedEvent, args);
+
+            base.ExecuteCommand(parameter);
+        }
+    }
+}
