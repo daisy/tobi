aba3b552be92fbb44281fb936a439b696c54fe2c
marisa-demeglio <marisa-demeglio@0210928e-6ab4-4709-a5c3-da57979d938b>
2009-07-04 02:17:27 +0000

    Metadata now revalidated when the Name changes.  However, I'm not totally happy with this solution.  I'm starting to think that each Metadata item has to be treated as the object that is bound to and that emits notifications, rather than handling Name and Content separately.
    Problems that remain:
    1) Slight hack, depending on your point of view, of how OnPropertyChanged is used to force a re-validation when the Name changes.
    2) Name changes/adds do not appear properly boldfaced
    3) UI not very attractive
    4) Assigning a different color to ListView's selected items did not work

---
+++ trunk/tobiapp/Tobi.Modules.MetadataPane/MetadataTemplateSelectors.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using urakawa.metadata.daisy;
+
+
+namespace Tobi.Modules.MetadataPane
+{
+    public class ContentTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DefaultTemplate { get; set; }
+
+        private MetadataPaneView m_View;
+        public ContentTemplateSelector(MetadataPaneView view)
+        {
+            if (view == null) return;
+            m_View = view;
+            DefaultTemplate = (DataTemplate)m_View.list.Resources["ContentTemplate"];
+        }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            return DefaultTemplate;
+        }
+    }
+
+    public class NameTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate OptionalTemplate { get; set; }
+        public DataTemplate RecommendedTemplate { get; set; }
+        public DataTemplate RequiredTemplate { get; set; }
+
+        private MetadataPaneView m_View;
+        public NameTemplateSelector(MetadataPaneView view)
+        {
+            if (view == null) return;
+
+            m_View = view;
+            OptionalTemplate = (DataTemplate)m_View.list.Resources["OptionalName"];
+            RecommendedTemplate = (DataTemplate)m_View.list.Resources["OptionalName"];
+            RequiredTemplate = (DataTemplate)m_View.list.Resources["RequiredName"];
+        }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+
+            NotifyingMetadataItem metadata = (NotifyingMetadataItem)item;
+            int index = SupportedMetadata_Z39862005.MetadataList.FindIndex(0, s => s.Name == metadata.Name);
+
+            if (index != -1)
+            {
+                MetadataDefinition definition = SupportedMetadata_Z39862005.MetadataList[index];
+
+                if (definition.Occurrence == MetadataOccurrence.Required)
+                    return RequiredTemplate;
+                else if (definition.Occurrence == MetadataOccurrence.Recommended)
+                    return RecommendedTemplate;
+            }
+            //if the occurrence is optional, or if we don't recognize this metadata
+            return OptionalTemplate;
+        }
+    }
+
+}
