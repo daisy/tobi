372c1f5bff0aff6892cd8c5d45506c80b91b6c73
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2009-07-18 18:35:47 +0000

    added a dim functionality for modal dialogs

--- trunk/tobiapp/ShellPresenter.cs
+++ trunk/tobiapp/ShellPresenter.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Media;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using Microsoft.Practices.Composite.Events;
@@ -14,9 +15,40 @@ using Microsoft.Practices.Unity;
 using Tobi.Infrastructure;
 using Tobi.Infrastructure.Commanding;
 using Tobi.Infrastructure.UI;
+using Application = System.Windows.Application;
 
 namespace Tobi
 {
+    public class DimAdorner : Adorner
+    {
+        private Pen m_pen;
+        private Rect m_rectRect;
+
+        public DimAdorner(UIElement adornedElement)
+            : base(adornedElement)
+        {
+            m_pen = new Pen(Brushes.Black, 1);
+            m_pen.Freeze();
+            m_rectRect = new Rect(new Point(0, 0), new Size(1, 1));
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            m_rectRect.Size = DesiredSize;
+
+            var fe = AdornedElement as FrameworkElement;
+            if (fe != null)
+            {
+                m_rectRect.Width = fe.ActualWidth;
+                m_rectRect.Height = fe.ActualHeight;
+            }
+
+            drawingContext.PushOpacity(0.4);
+            drawingContext.DrawRectangle(Brushes.Black, m_pen, m_rectRect);
+            drawingContext.Pop();
+        }
+    }
+
     public class ShellPresenter : IShellPresenter
     {
         private void playAudioCue(string audioClipName)
@@ -633,6 +665,24 @@ namespace Tobi
             foreach (var command in m_listOfIconRichCommands)
             {
                 command.IconDrawScale = value;
+            }
+        }
+
+        public void DimBackgroundWhile(Action action)
+        {
+            var aLayer = AdornerLayer.GetAdornerLayer((Visual)View.Window.Content);
+
+            var theAdorner = new DimAdorner((UIElement)View.Window.Content);
+            if (aLayer != null)
+            {
+                aLayer.Add(theAdorner);
+            }
+
+            action.Invoke();
+
+            if (aLayer != null)
+            {
+                aLayer.Remove(theAdorner);
             }
         }
 
