489a00d496958908d0ef811912bb351b02253cd8
marisa-demeglio <marisa-demeglio@0210928e-6ab4-4709-a5c3-da57979d938b>
2009-08-03 19:37:10 +0000

    Adding WPFToolkit library, which contains the code for datagrid (used in the metadata editor).

---
+++ trunk/tobiapp/ExternalLibs/WPFToolkit/DataGrid/Microsoft/Windows/Automation/Peers/DataGridRowsPresenterAutomationPeer.cs
@@ -0,0 +1,89 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+using System.Windows.Controls;
+using Microsoft.Windows.Controls;
+using Microsoft.Windows.Controls.Primitives;
+
+namespace Microsoft.Windows.Automation.Peers
+{
+    /// <summary>
+    /// AutomationPeer for DataGridRowsPresenter
+    /// </summary>
+    public sealed class DataGridRowsPresenterAutomationPeer : FrameworkElementAutomationPeer
+    {
+        #region Constructors
+
+        /// <summary>
+        /// AutomationPeer for DataGridColumnHeadersPresenter
+        /// </summary>
+        /// <param name="owner">DataGridColumnHeadersPresenter</param>
+        public DataGridRowsPresenterAutomationPeer(DataGridRowsPresenter owner)
+            : base(owner)
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        private DataGridAutomationPeer DataGridPeer
+        {
+            get
+            {
+                if (this.OwningRowsPresenter.Owner != null)
+                {
+                    return CreatePeerForElement(this.OwningRowsPresenter.Owner) as DataGridAutomationPeer;
+                }
+
+                return null;
+            }
+        }
+
+        private DataGridRowsPresenter OwningRowsPresenter
+        {
+            get
+            {
+                return (DataGridRowsPresenter)Owner;
+            }
+        }
+
+        #endregion
+
+        #region AutomationPeer Overrides
+
+        ///
+        protected override List<AutomationPeer> GetChildrenCore()
+        {
+            if (DataGridPeer != null)
+            {
+                return DataGridPeer.GetItemPeers();
+            }
+
+            return base.GetChildrenCore();
+        }
+
+        /// <summary>
+        /// Called by GetClassName that gets a human readable name that, in addition to AutomationControlType, 
+        /// differentiates the control represented by this AutomationPeer.
+        /// </summary>
+        /// <returns>The string that contains the name.</returns>
+        protected override string GetClassNameCore()
+        {
+            return Owner.GetType().Name;
+        }
+
+        /// <summary>
+        /// Gets a value that specifies whether the element is a content element.
+        /// </summary>
+        /// <returns>true if the element is a content element; otherwise false</returns>
+        protected override bool IsContentElementCore()
+        {
+            return false;
+        }
+
+        #endregion
+    }
+}
