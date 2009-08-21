ee5954def899adc4da31b26f76a27979b067b9ab
Marisa DeMeglio <marisa.demeglio@gmail.com>
2009-08-21 07:23:05 +0000

    experimented with datepicker - too picky about formatting - also keyboard shortcuts don't work in this release.  in the end, it was removed.  improved remove button logic

--- tobiapp/Tobi.Modules.MetadataPane/MetadataPaneView.xaml.cs
+++ tobiapp/Tobi.Modules.MetadataPane/MetadataPaneView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Collections.ObjectModel;
 using urakawa.metadata.daisy;
 using Microsoft.Windows.Controls;
+
 namespace Tobi.Modules.MetadataPane
 {
     /// <summary>
@@ -112,116 +113,4 @@ namespace Tobi.Modules.MetadataPane
         }
     }
     
-    public class IsNotRequiredOccurrenceConverter : IValueConverter
-    {
-        //return false if required
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            if (value == null) return false;
-            NotifyingMetadataItem item = (NotifyingMetadataItem) value;
-            if (item.Definition != null && item.Definition.Occurrence == MetadataOccurrence.Required) return false;
-            else return true;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException
-                ("The ConvertBack method is not implemented because this Converter should only be used in a one-way Binding.");
-        }
-    }
-    public class OccurrenceDescriptionConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            if (value == null) return "";
-            MetadataDefinition item = (MetadataDefinition)value;
-            if (item.Occurrence == MetadataOccurrence.Required)
-                return "Required.  ";
-            else if (item.Occurrence == MetadataOccurrence.Recommended)
-                return "Recommended. ";
-            else
-                return "Optional. ";
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException
-                ("The ConvertBack method is not implemented because this Converter should only be used in a one-way Binding.");
-        }
-    }
-    public class RemoveReadOnlyErrorsConverter : IValueConverter
-    {
-        //don't include errors about read-only metadata items
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            ObservableCollection<MetadataValidationError> errors = new ObservableCollection<MetadataValidationError>();
-            ObservableCollection<MetadataValidationError> sourceList =
-                (ObservableCollection<MetadataValidationError>) value;
-            foreach (MetadataValidationError error in sourceList)
-            {
-                if (error.Definition.IsReadOnly == false)
-                    errors.Add(error);
-            }
-
-            return errors;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException
-                ("The ConvertBack method is not implemented because this Converter should only be used in a one-way Binding.");
-        }
-    }
-    public class DescriptiveErrorTextConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            if (value == null) return null;
-            MetadataValidationError error = (MetadataValidationError) value;
-            string description = null;
-            if (error is MetadataValidationFormatError)
-            {
-                description = string.Format("{0} must be {1}.", 
-                    error.Definition.Name, 
-                    ((MetadataValidationFormatError)error).Hint);    
-            }
-            else if (error is MetadataValidationMissingItemError)
-            {
-                description = string.Format("Missing {0}", error.Definition.Name);
-            }
-            else if (error is MetadataValidationDuplicateItemError)
-            {
-                description = string.Format("Duplicate of {0} not allowed.", error.Definition.Name);
-            }
-            else
-            {
-                description = string.Format("Unspecified error in {0}.", error.Definition.Name);
-            }
-            return description;            
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException
-                ("The ConvertBack method is not implemented because this Converter should only be used in a one-way Binding.");
-        }
-    }
-    public class ValidationStatusTextConverter : IValueConverter
-    {
-        private static string NoErrorsFound = "All metadata is valid.";
-        private static string ErrorsFound = "Please correct the following errors:";
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            if (value == null || (int)value == 0)
-                return NoErrorsFound;
-            else
-                return ErrorsFound;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException
-                ("The ConvertBack method is not implemented because this Converter should only be used in a one-way Binding.");
-        }
-    }
 }
