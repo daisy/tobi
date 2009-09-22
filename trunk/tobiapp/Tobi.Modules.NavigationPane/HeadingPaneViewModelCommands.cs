0bbc680d2b70063b05ec8bec9ff6889130a3ebf8
tom-mcartney <tom-mcartney@0210928e-6ab4-4709-a5c3-da57979d938b>
2009-09-22 19:05:08 +0000

    I added an "Edit Heading" command for editing the nodes in the TOC view.

--- trunk/tobiapp/Tobi.Modules.NavigationPane/HeadingPaneViewModelCommands.cs
+++ trunk/tobiapp/Tobi.Modules.NavigationPane/HeadingPaneViewModelCommands.cs
@@ -3,13 +3,17 @@ using System.Windows.Media;
 using Microsoft.Practices.Composite.Logging;
 using Tobi.Common;
 using Tobi.Common.MVVM.Command;
+using urakawa.core;
 
 namespace Tobi.Modules.NavigationPane
 {
     public partial class HeadingPaneViewModel
     {
         public static RichDelegateCommand<object> CommandExpandAll { get; private set; }
+//        public static RichDelegateCommand<object> CommandExpand { get; private set; }
         public static RichDelegateCommand<object> CommandCollapseAll { get; private set; }
+//        public static RichDelegateCommand<object> CommandCollapse { get; private set; }
+        public static RichDelegateCommand<object> CommandEditText { get; private set; }
 
         private void intializeCommands()
         {
@@ -23,6 +27,15 @@ namespace Tobi.Modules.NavigationPane
                 shellPresenter.LoadTangoIcon("list-add"),
                 obj => _headingsNavigator.ExpandAll(),
                 obj => _headingsNavigator != null);
+/*
+            CommandExpand = new RichDelegateCommand<object>(
+                UserInterfaceStrings.TreeExpand,
+                UserInterfaceStrings.TreeExpand_,
+                null,
+                shellPresenter.LoadGnomeNeuIcon("Neu_list-add"),
+                obj => _headingsNavigator.Expand((HeadingTreeNodeWrapper)obj),
+                obj => (_headingsNavigator != null && (obj as HeadingTreeNodeWrapper).HasChildren && !(obj as HeadingTreeNodeWrapper).IsExpanded));
+ */
             //
             CommandCollapseAll = new RichDelegateCommand<object>(
                 UserInterfaceStrings.TreeCollapseAll,
@@ -31,8 +44,28 @@ namespace Tobi.Modules.NavigationPane
                 shellPresenter.LoadTangoIcon("list-remove"),
                 obj => _headingsNavigator.CollapseAll(),
                 obj => _headingsNavigator != null);
+/*
+            CommandCollapse = new RichDelegateCommand<object>(
+                UserInterfaceStrings.TreeCollapse,
+                UserInterfaceStrings.TreeCollapse_,
+                null,
+                shellPresenter.LoadGnomeNeuIcon("Neu_list-remove"),
+                obj => _headingsNavigator.Collapse((HeadingTreeNodeWrapper)obj),
+                obj => (_headingsNavigator != null && (obj as HeadingTreeNodeWrapper).HasChildren && (obj as HeadingTreeNodeWrapper).IsExpanded));
+*/
+            CommandEditText = new RichDelegateCommand<object>(
+                UserInterfaceStrings.TreeEdit,
+                UserInterfaceStrings.TreeEdit_,
+                null,
+                shellPresenter.LoadTangoIcon("accessories-text-editor"),
+                obj => _headingsNavigator.EditText((HeadingTreeNodeWrapper)obj),
+                obj => _headingsNavigator!=null);
 
+            shellPresenter.RegisterRichCommand(CommandExpandAll);
+//            shellPresenter.RegisterRichCommand(CommandExpand);
             shellPresenter.RegisterRichCommand(CommandCollapseAll);
+//            shellPresenter.RegisterRichCommand(CommandCollapse);
+            shellPresenter.RegisterRichCommand(CommandEditText);
         }
     }
 }
