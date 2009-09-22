f0b9c23a78181c730db58fb65786a2ea3166a6e0
tom-mcartney <tom-mcartney@0210928e-6ab4-4709-a5c3-da57979d938b>
2009-09-22 18:53:43 +0000

    This form is used to edit a heading from the TOC Tree View.

---
+++ trunk/tobiapp/Tobi.Modules.NavigationPane/InputBox.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tobi.Modules.NavigationPane
+{
+    /// <summary>
+    /// Interaction logic for InputBox.xaml
+    /// </summary>
+    public partial class InputBox : UserControl
+    {
+        public InputBox(string Caption, string Value)
+        {
+            InitializeComponent();
+            tbInput.Text = Value;
+            lblCaption.Content = Caption;
+        }
+        public InputBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
