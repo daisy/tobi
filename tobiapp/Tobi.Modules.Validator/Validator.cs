fdb2a9066e50b87bdc551cd6c1160282952fdcd4
Marisa DeMeglio <marisa.demeglio@gmail.com>
2009-11-15 19:55:41 +0000

    New validator class

---
+++ tobiapp/Tobi.Modules.Validator/Validator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tobi.Common.Validation;
+
+namespace Tobi.Modules.Validator
+{
+    public class Validator
+    {
+        public bool Validate()
+        {
+            bool isValid = true;
+            ValidationItems.Clear();
+            foreach (IValidator v in Validators)
+            {
+                isValid = isValid && v.Validate();
+                if (!isValid)
+                {
+                    foreach (ValidationItem item in v.ValidationItems)
+                    {
+                        ValidationItems.Add(item);
+                    }
+                }
+            }
+            return isValid;
+        }
+    
+        public void OnProjectLoaded()
+        {
+            Validate();
+        }
+
+        public void OnUndoRedo()
+        {
+            Validate();
+        }
+    }
+}
