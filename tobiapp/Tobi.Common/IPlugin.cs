16683727a8f314523b393d7b1825021151658680
Daniel Weck <625201+danielweck@users.noreply.github.com>
2009-11-19 17:09:25 +0000

    added the plugin interface - testing the latest version of Visual SVN (with Tortoise)

---
+++ tobiapp/Tobi.Common/IPlugin.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Practices.Composite.Modularity;
+
+namespace Tobi.Common
+{
+    public interface IPlugin : IModule
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+    }
+}
