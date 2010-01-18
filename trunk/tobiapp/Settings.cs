d2691e12e15ee7296f30e1357fce36990980e1aa
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2010-01-18 05:01:25 +0000

    moved keyboard shortcuts settings to a separate storage space, so that they get grouped in the user.config overrides, and so that errors generated by the Visual Studio tooling (due to bad handling of KeyGesture serialization) doesn't impact the main settings (which are based on non-custom types, such as Double and Boolean)

--- trunk/tobiapp/Settings.cs
+++ trunk/tobiapp/Settings.cs
@@ -23,7 +23,26 @@ namespace Tobi {
             get { return Tobi.Settings.Default; }
         }
     }
-    
+
+    [Export(typeof(ISettingsProvider)), PartCreationPolicy(CreationPolicy.Shared)]
+    public class SettingsProvider_KeyGestures : ISettingsProvider, IPartImportsSatisfiedNotification
+    {
+
+#pragma warning disable 1591 // non-documented method
+        public void OnImportsSatisfied()
+#pragma warning restore 1591
+        {
+            //#if DEBUG
+            //            Debugger.Break();
+            //#endif
+        }
+
+        public ApplicationSettingsBase Settings
+        {
+            get { return Settings_KeyGestures.Default; }
+        }
+    }
+
     // This class allows you to handle specific events on the settings class:
     //  The SettingChanging event is raised before a setting's value is changed.
     //  The PropertyChanged event is raised after a setting's value is changed.
@@ -35,6 +54,23 @@ namespace Tobi {
             PropertyChanged +=new System.ComponentModel.PropertyChangedEventHandler(Settings_PropertyChanged);
         }
         //~Settings()
+        //{
+        //    Save();
+        //}
+        private void Settings_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            //TODO: 
+        }
+    }
+
+    internal sealed partial class Settings_KeyGestures
+    {
+
+        public Settings_KeyGestures()
+        {
+            PropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(Settings_PropertyChanged);
+        }
+        //~Settings_KeyGestures()
         //{
         //    Save();
         //}
