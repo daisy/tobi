c499e8667a1ab5977ee3021c237bf1e6578a452e
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2010-01-21 15:17:26 +0000

    updating toolbar settings.cs file

--- tobiapp/Tobi.Modules.ToolBars/Settings.cs
+++ tobiapp/Tobi.Modules.ToolBars/Settings.cs
@@ -23,6 +23,26 @@ namespace Tobi.Plugin.ToolBars
             get { return ToolBars.Settings.Default; }
         }
     }
+
+    [Export ( typeof ( ISettingsProvider ) ), PartCreationPolicy ( CreationPolicy.Shared )]
+    public class SettingsProvider_KeyGestures : ISettingsProvider, IPartImportsSatisfiedNotification
+        {
+
+#pragma warning disable 1591 // non-documented method
+        public void OnImportsSatisfied ()
+#pragma warning restore 1591
+            {
+            //#if DEBUG
+            //            Debugger.Break();
+            //#endif
+            }
+
+        public ApplicationSettingsBase Settings
+            {
+            get { return Tobi.Plugin.ToolBars.Settings_KeyGestures.Default; }
+            }
+        }
+
     // This class allows you to handle specific events on the settings class:
     //  The SettingChanging event is raised before a setting's value is changed.
     //  The PropertyChanged event is raised after a setting's value is changed.
@@ -44,4 +64,22 @@ namespace Tobi.Plugin.ToolBars
             //TODO: 
         }
     }
+
+    internal sealed partial class Settings_KeyGestures
+        {
+        public Settings_KeyGestures ()
+            {
+            //PropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(Settings_PropertyChanged);
+            }
+        //~Settings_KeyGestures()
+        //{
+        //    Save();
+        //}
+        private void Settings_PropertyChanged ( object sender, PropertyChangedEventArgs e )
+            {
+            //TODO: 
+            }
+        }
+
+
 }
