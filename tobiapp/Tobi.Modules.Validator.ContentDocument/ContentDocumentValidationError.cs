2b40d717cb0f1e3c6170c6a3e03996a751dab6d0
Marisa DeMeglio <marisa.demeglio@gmail.com>
2010-01-31 06:19:19 +0000

    First pass at integrating document validation in Tobi.  Is currently passive -- not triggered by anything yet

---
+++ tobiapp/Tobi.Modules.Validator.ContentDocument/ContentDocumentValidationError.cs
@@ -0,0 +1,35 @@
+﻿using Tobi.Common.Validation;
+using urakawa.core;
+
+namespace Tobi.Plugin.Validator.ContentDocument
+{
+    public enum ContentDocumentErrorType
+    {
+        UndefinedElement,
+        ElementMisuse
+    }
+
+    public class ContentDocumentValidationError : ValidationItem
+    {
+        public ContentDocumentErrorType ErrorType { get; set; }
+        public string AllowedChildNodes { get; set; }
+        public TreeNode Target { get; set;}
+
+        public ContentDocumentValidationError()
+        {
+            Severity = ValidationSeverity.Error;
+        }
+
+        public ContentDocumentValidationError(TreeNode target, ContentDocumentErrorType errorType, 
+            string allowedChildNodes, string message)
+        {
+            Target = target;
+            ErrorType = errorType;
+            AllowedChildNodes = allowedChildNodes;
+            Message = message;
+
+            Severity = ValidationSeverity.Error;
+        }
+       
+    }
+}
---
+++ tobiapp/Tobi.Modules.Validator.ContentDocument/DtdCache.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace UrakawaDomValidation
+{
+    public class DtdCache
+    {
+        /// <summary>
+        ///  Read from cache into a hashtable
+        /// 
+        /// Cache file format:
+        /// line 1: Element name
+        /// line 2: Regex string of element's allowed children
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        public static Hashtable ReadFromCache(StreamReader reader)
+        {
+            Hashtable hashtable = new Hashtable();
+            string name = reader.ReadLine();
+            string regExpStr = reader.ReadLine();
+
+            while (name != null && regExpStr != null)
+            {
+                Regex regEx = new Regex(regExpStr);
+                hashtable[name] = regEx;
+                name = reader.ReadLine();
+                regExpStr = reader.ReadLine();
+            }
+            reader.Close();
+            return hashtable;
+        }
+
+        /// <summary>
+        /// Write hashtable data to cache
+        /// 
+        /// The hashtable is formatted like:
+        /// key: element name
+        /// value: regex (object) of element's allowed children
+        /// 
+        /// Cache file format:
+        /// line 1: Element name
+        /// line 2: Regex string of element's allowed children
+        /// </summary>
+        /// <param name="hashtable"></param>
+        /// <param name="writer"></param>
+        public static void WriteToCache(Hashtable hashtable, StreamWriter writer)
+        {
+            foreach (DictionaryEntry entry in hashtable)
+            {
+                string name = (string)entry.Key;
+                string regExpStr = ((Regex)entry.Value).ToString();
+                writer.WriteLine(name);
+                writer.WriteLine(regExpStr);
+            }
+            writer.Close();
+        }
+    }
+}
