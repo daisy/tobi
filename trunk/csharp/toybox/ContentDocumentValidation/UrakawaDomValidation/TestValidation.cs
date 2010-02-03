68b699d3d083944bfa23e2c99a8736062c9a3684
marisa-demeglio <marisa-demeglio@0210928e-6ab4-4709-a5c3-da57979d938b>
2010-02-03 02:26:46 +0000

    removed ?: from regex generator for testing groups.  added feature to enter element + child data via cmd line.  looking for the most specific error indicator(s) when something goes wrong in the document structure.

--- trunk/csharp/toybox/ContentDocumentValidation/UrakawaDomValidation/TestValidation.cs
+++ trunk/csharp/toybox/ContentDocumentValidation/UrakawaDomValidation/TestValidation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 using urakawa;
 using urakawa.core;
 using urakawa.xuk;
@@ -17,9 +18,7 @@ namespace UrakawaDomValidation
             string paintersbook = @"..\..\..\greatpainters.xuk";
 
             //invalid files
-            //it has two frontmatter items whereas only zero or one is allowed.
             string simplebook_invalid_doublefrontmatter = @"..\..\..\simplebook-invalid-doublefrontmatter.xuk";
-            //these both register as invalid, and for the right reasons
             string simplebook_invalid_unrecognized_element = @"..\..\..\simplebook-invalid-element.xuk";
             string simplebook_invalid_badnesting = @"..\..\..\simplebook-invalid-badnesting.xuk";
 
@@ -27,7 +26,8 @@ namespace UrakawaDomValidation
             //eventually, we'll need a table of dtd to cached version)
             string dtdcache = @"..\..\..\dtd.cache";
 
-            bool writeToLog = true;
+            bool writeToLog = false;
+            bool reportTraces = false;
 
             FileStream ostrm = null;
             StreamWriter writer = null;
@@ -54,7 +54,7 @@ namespace UrakawaDomValidation
                 validator.SaveCachedDtd(new StreamWriter(Path.GetFullPath(dtdcache)));
             }
 
-            Project project = LoadXuk(paintersbook);
+            Project project = LoadXuk(simplebook_invalid_doublefrontmatter);
             TreeNode root = project.Presentations.Get(0).RootNode;
             bool res = validator.Validate(root);
 
@@ -62,12 +62,19 @@ namespace UrakawaDomValidation
 
             foreach (UrakawaDomValidationReportItem item in validator.ValidationReportItems)
             {
-                string type = item.ItemType == UrakawaDomValidationReportItem.ReportItemType.Trace ? "TRACE" : "ERROR";
-                Console.WriteLine(string.Format("* {0}", type));
-                Console.WriteLine(string.Format("Node name: {0}", item.Node.GetXmlElementQName().LocalName));
-                Console.WriteLine(string.Format("Reg ex: {0}", validator.GetRegex(item.Node)));
-                Console.WriteLine(item.Message);
-                Console.WriteLine("\n");
+                if (item.ItemType == UrakawaDomValidationReportItem.ReportItemType.Error ||
+                    (item.ItemType == UrakawaDomValidationReportItem.ReportItemType.Trace &&
+                    reportTraces == true))
+                {
+                    string type = item.ItemType == UrakawaDomValidationReportItem.ReportItemType.Trace
+                                      ? "TRACE"
+                                      : "ERROR";
+                    Console.WriteLine(string.Format("* {0}", type));
+                    Console.WriteLine(string.Format("Node name: {0}", item.Node.GetXmlElementQName().LocalName));
+                    Console.WriteLine(string.Format("Reg ex: {0}", validator.GetRegex(item.Node)));
+                    Console.WriteLine(item.Message);
+                    Console.WriteLine("\n");
+                }
             }
             if (writeToLog)
             {
@@ -76,6 +83,9 @@ namespace UrakawaDomValidation
                 ostrm.Close();
                 Console.WriteLine(res ? "VALID!!" : "INVALID!!");
             }
+
+            TestRegex(validator);
+
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
         }
@@ -88,6 +98,64 @@ namespace UrakawaDomValidation
             var action = new OpenXukAction(project, uri);
             action.Execute();
             return project;
+        }
+        public static void TestRegex(UrakawaDomValidation validator)
+        {
+            Console.WriteLine("Enter an element name:");
+            string elm = Console.ReadLine();
+
+            string regexStr = validator.GetRegex(elm);
+            if (string.IsNullOrEmpty(regexStr))
+            {
+                Console.WriteLine("Element definition not found!");
+                return;
+            }
+
+            
+            Console.WriteLine("Enter the children for this element, separated by a comma:");
+            string children = Console.ReadLine();
+            children = children.Replace(", ", "#");
+            children = children.Replace(",", "#");
+            if (!children.EndsWith("#")) children += "#";
+            Console.WriteLine(children);
+            Regex regex = new Regex(regexStr);
+
+            Match match = regex.Match(children);
+            foreach (Group g in match.Groups)
+            {
+                Console.WriteLine(string.Format("Group: {0}", g.ToString()));
+            }
+            if (match.Success == true && match.ToString() == children)
+            {
+                Console.WriteLine("SUCCESS!");
+                return;
+            }
+
+            if (match.Success == false)
+            {
+                Console.WriteLine("No match!");
+                Console.WriteLine(regex.ToString());
+                Console.WriteLine(string.Format("Assume that {0} are not allowed children.", children));
+                return;
+            }
+
+            Console.WriteLine(string.Format("Match: {0}", match.ToString()));
+            
+            if (match.ToString() != children)
+            {
+                string[] childrenArr = children.Split('#');
+                foreach (string child in childrenArr)
+                {
+                    if (match.ToString().IndexOf(child) == -1)
+                    {
+                        Console.WriteLine(string.Format("{0} not found in match", child));
+                    }
+                    if (regex.ToString().IndexOf(child) == -1)
+                    {
+                        Console.WriteLine(string.Format("{0} not found in regex", child));
+                    }
+                }                
+            }
         }
     }
 }
