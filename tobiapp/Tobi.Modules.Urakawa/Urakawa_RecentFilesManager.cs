20fc1155b5682bac42002e08a4bcc7f37c6ce907
rachana-singh <rachana-singh@0210928e-6ab4-4709-a5c3-da57979d938b>
2010-02-13 08:54:37 +0000

    Recent files modified a little more.

--- tobiapp/Tobi.Modules.Urakawa/Urakawa_RecentFilesManager.cs
+++ tobiapp/Tobi.Modules.Urakawa/Urakawa_RecentFilesManager.cs
@@ -6,41 +6,68 @@ using System.Text;
 using Tobi.Common.MVVM.Command;
 
 namespace Tobi.Plugin.Urakawa
-{
-    public partial class UrakawaSession
     {
-        private List<string> m_recent_files_list = new List<string>();
-        public void Add(string file_path)
+    public partial class UrakawaSession
         {
-            if (m_recent_files_list.Contains(file_path))
-            {
-                m_recent_files_list.Add(file_path);
-                Save();
-            }
-        }
-        public void Save()
-        {
-            string mydocpath = System.AppDomain.CurrentDomain.BaseDirectory + "\\TobiRecentFilesList.txt";
 
-            StreamWriter writeText = File.CreateText(mydocpath);
-            for (int i = 0; i < m_recent_files_list.Count; i++)
-            {
-                writeText.WriteLine(m_recent_files_list[i]);
-            }
-            writeText.Close();
-        }
-
-        public void Clear()
-        {
-            m_recent_files_list.Clear();
-            Save();
-        }
-
+        private string m_File_Save_Path = null;
+        private List<string> m_recent_files_list = new List<string> ();
+        
         public List<string> RecentFilesList
         {
-            get { return m_recent_files_list; }
+            get 
+                {
+                if (m_recent_files_list.Count == 0)
+                    {
+                    InitializeRecentFilesList ();
+                    }
+                return m_recent_files_list; 
+                }
+            }
 
+        public void InitializeRecentFilesList ()
+            {
+            m_File_Save_Path = System.AppDomain.CurrentDomain.BaseDirectory + "\\TobiRecentFilesList.txt"; // can store it in better place
+
+            if (File.Exists ( m_File_Save_Path ))
+                {
+                StreamReader sr = new StreamReader ( m_File_Save_Path );
+                string fullFilePath = null ;
+
+                while ((fullFilePath = sr.ReadLine () ) != null)
+                    {
+                     if ( !m_recent_files_list.Contains (fullFilePath ))
+                         m_recent_files_list.Add ( fullFilePath );
+                    }
+                }
         }
-        
+
+
+        public void AddToRecentFilesList ( string file_path )
+            {
+            if (m_recent_files_list.Contains ( file_path ))
+                {
+                m_recent_files_list.Add ( file_path );
+                SaveRecentFilesList ();
+                }
+            }
+
+
+        public void SaveRecentFilesList ()
+            {
+            
+            StreamWriter writeText = File.CreateText ( m_File_Save_Path);
+            for (int i = 0; i < m_recent_files_list.Count; i++)
+                {
+                writeText.WriteLine ( m_recent_files_list[i] );
+                }
+            writeText.Close ();
+            }
+
+        public void Clear ()
+            {
+            m_recent_files_list.Clear ();
+            SaveRecentFilesList ();
+            }
+        }
     }
-}
