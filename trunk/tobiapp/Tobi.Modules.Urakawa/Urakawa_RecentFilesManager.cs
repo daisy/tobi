e6d52ab406249bb8af19c6dd728d07e5dd9f4922
avneesh-singh <avneesh-singh@0210928e-6ab4-4709-a5c3-da57979d938b>
2010-02-17 07:30:04 +0000

    recent files should be added to user domain isolated storage

--- trunk/tobiapp/Tobi.Modules.Urakawa/Urakawa_RecentFilesManager.cs
+++ trunk/tobiapp/Tobi.Modules.Urakawa/Urakawa_RecentFilesManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Text;
 using Tobi.Common.MVVM.Command;
@@ -10,37 +11,51 @@ namespace Tobi.Plugin.Urakawa
     public partial class UrakawaSession
         {
 
-        private string m_File_Save_Path = null;
+        private string m_RecentFilesListStorageFileName = "Tobi_RecentFiles.txt";
         private List<string> m_recent_files_list = new List<string> ();
-        
+
         public List<string> RecentFilesList
-        {
-            get 
+            {
+            get
                 {
                 if (m_recent_files_list.Count == 0)
                     {
                     InitializeRecentFilesList ();
                     }
-                return m_recent_files_list; 
+                return m_recent_files_list;
                 }
             }
 
         public void InitializeRecentFilesList ()
             {
-            m_File_Save_Path = System.AppDomain.CurrentDomain.BaseDirectory + "\\TobiRecentFilesList.txt"; // can store it in better place
+            IsolatedStorageFileStream storageStream = null;
 
-            if (File.Exists ( m_File_Save_Path ))
+            IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication ();
+
+            if (store.GetFileNames ( m_RecentFilesListStorageFileName ).Length > 0)
                 {
-                StreamReader sr = new StreamReader ( m_File_Save_Path );
-                string fullFilePath = null ;
 
-                while ((fullFilePath = sr.ReadLine () ) != null)
-                    {
-                     if ( !m_recent_files_list.Contains (fullFilePath ))
-                         m_recent_files_list.Add ( fullFilePath );
-                    }
+                storageStream =
+                    new IsolatedStorageFileStream ( m_RecentFilesListStorageFileName, FileMode.Open, FileAccess.Read, FileShare.None, store );
                 }
-        }
+
+
+            if (storageStream != null)
+                {
+                StreamReader sr = new StreamReader ( storageStream );
+                string fullFilePath = null;
+
+                while ((fullFilePath = sr.ReadLine ()) != null)
+                    {
+                    if (!m_recent_files_list.Contains ( fullFilePath ))
+                        m_recent_files_list.Add ( fullFilePath );
+
+                    }
+                sr.Close ();
+                storageStream.Close ();
+                store.Close ();
+                }
+            }
 
 
         public void AddToRecentFilesList ( string file_path )
@@ -55,13 +70,23 @@ namespace Tobi.Plugin.Urakawa
 
         public void SaveRecentFilesList ()
             {
-            
-            StreamWriter writeText = File.CreateText ( m_File_Save_Path);
+            IsolatedStorageFileStream storageStream = null;
+
+            IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication ();
+
+            storageStream =
+           new IsolatedStorageFileStream ( m_RecentFilesListStorageFileName, FileMode.Create, store );
+
+
+            StreamWriter writeText = new StreamWriter ( storageStream );
+
             for (int i = 0; i < m_recent_files_list.Count; i++)
                 {
                 writeText.WriteLine ( m_recent_files_list[i] );
                 }
             writeText.Close ();
+            storageStream.Close ();
+            store.Close ();
             }
 
         public void Clear ()
