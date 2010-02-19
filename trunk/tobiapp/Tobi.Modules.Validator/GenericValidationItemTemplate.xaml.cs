580bb5dc2a7617f3650c2ba8ef19d12aa81752f6
marisa-demeglio <marisa-demeglio@0210928e-6ab4-4709-a5c3-da57979d938b>
2010-02-19 00:15:36 +0000

    more experiments with ValidationItem data templates, and a few experiemnts with document visuals

--- trunk/tobiapp/Tobi.Modules.Validator/GenericValidationItemTemplate.xaml.cs
+++ trunk/tobiapp/Tobi.Modules.Validator/GenericValidationItemTemplate.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Tobi.Plugin.Validator
 {
@@ -21,7 +9,6 @@ namespace Tobi.Plugin.Validator
     {
         public GenericValidationItemTemplate()
         {
-            InitializeComponent();
         }
     }
 }
