7343a746a83db79eb9d91109bb3639032ef635e6
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2010-02-22 23:02:01 +0000

    updated MEF to preview 9

--- trunk/tobiapp/ExternalLibs/MEF/AssemblyInfo.cs
+++ trunk/tobiapp/ExternalLibs/MEF/AssemblyInfo.cs
@@ -1,11 +1,9 @@
-﻿using System.Runtime.InteropServices;
 //------------------------------------------------------------
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 //------------------------------------------------------------
 [assembly: System.CLSCompliant(true)]
 [assembly: System.Reflection.AssemblyTitle("System.ComponentModel.Composition")]
 [assembly: System.Reflection.AssemblyCopyright("(c) Microsoft Corporation. All rights reserved.")]
-[assembly: System.Reflection.AssemblyVersion("2009.22.10")]
+[assembly: System.Reflection.AssemblyVersion("2010.02.11")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("System.ComponentModel.Composition.UnitTests")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("System.ComponentModel.Composition.UnitTestFramework")]
-[assembly: ComVisibleAttribute(false)]
