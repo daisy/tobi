6d9efbf6bd5609330d79448619ecb18f04597123
tom-mcartney <1568944+GrayWolfMT@users.noreply.github.com>
2010-02-22 16:32:13 +0000

    New interface to define the global search commands.

---
+++ tobiapp/Tobi.Common/IGlobalSearchCommands.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tobi.Common.MVVM.Command;
+
+namespace Tobi.Common
+{
+    public interface IGlobalSearchCommands
+    {
+        RichCompositeCommand CmdFindNext { get; }
+        RichCompositeCommand CmdFindPrevious { get; }
+    }
+}
