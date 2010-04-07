91b3ea7d6de228532629906bd9118820ff8a1535
Daniel Weck <625201+danielweck@users.noreply.github.com>
2010-04-07 17:21:46 +0000

    missing audio validator is now hooked to the undo/redo events

--- tobiapp/Tobi.Modules.Validator.ContentDocument/ContentDocumentValidator.cs
+++ tobiapp/Tobi.Modules.Validator.ContentDocument/ContentDocumentValidator.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.IO;
 using System.Text.RegularExpressions;
-using System.Windows;
 using DtdSharp;
 using Microsoft.Practices.Composite.Logging;
 using Tobi.Common;
