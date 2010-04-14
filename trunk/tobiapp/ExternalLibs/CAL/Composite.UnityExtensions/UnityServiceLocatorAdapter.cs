a24db14776bf84b901af62dd020fe7f0c211631e
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2010-04-14 05:43:45 +0000

    Things are getting weird...I've restored the old Unity behaviour but Tobi still locks on the constructor invoke (reflection). Will try under Windowx XP to see if it is Win7-specific (x64 as well)

--- trunk/tobiapp/ExternalLibs/CAL/Composite.UnityExtensions/UnityServiceLocatorAdapter.cs
+++ trunk/tobiapp/ExternalLibs/CAL/Composite.UnityExtensions/UnityServiceLocatorAdapter.cs
@@ -62,4 +62,67 @@ namespace Microsoft.Practices.Composite.UnityExtensions
             return _unityContainer.ResolveAll(serviceType);
         }
     }
+
+
+    /// <summary>
+    /// An implementation of <see cref="IServiceLocator"/> that wraps a Unity container.
+    /// </summary>
+    public class UnityServiceLocator : ServiceLocatorImplBase, IDisposable
+    {
+        private IUnityContainer container;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnityServiceLocator"/> class for a container.
+        /// </summary>
+        /// <param name="container">The <see cref="IUnityContainer"/> to wrap with the <see cref="IServiceLocator"/>
+        /// interface implementation.</param>
+        public UnityServiceLocator(IUnityContainer container)
+        {
+            this.container = container;
+            container.RegisterInstance<IServiceLocator>(this, new ExternallyControlledLifetimeManager());
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        /// <filterpriority>2</filterpriority>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1816:CallGCSuppressFinalizeCorrectly",
+            Justification = "Object is not finalizable, no reason to call SuppressFinalize")]
+        public void Dispose()
+        {
+            if (container != null)
+            {
+                container.Dispose();
+                container = null;
+            }
+        }
+
+        /// <summary>
+        /// When implemented by inheriting classes, this method will do the actual work of resolving
+        ///             the requested service instance.
+        /// </summary>
+        /// <param name="serviceType">Type of instance requested.</param><param name="key">Name of registered service you want. May be null.</param>
+        /// <returns>
+        /// The requested service instance.
+        /// </returns>
+        protected override object DoGetInstance(Type serviceType, string key)
+        {
+            if (container == null) throw new ObjectDisposedException("container");
+            return container.Resolve(serviceType, key);
+        }
+
+        /// <summary>
+        /// When implemented by inheriting classes, this method will do the actual work of
+        ///             resolving all the requested service instances.
+        /// </summary>
+        /// <param name="serviceType">Type of service requested.</param>
+        /// <returns>
+        /// Sequence of service instance objects.
+        /// </returns>
+        protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+        {
+            if (container == null) throw new ObjectDisposedException("container");
+            return container.ResolveAll(serviceType);
+        }
+    }
 }
