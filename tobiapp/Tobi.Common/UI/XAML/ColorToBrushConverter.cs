595f081ff551c4aa5e31fb3ef5548443ee32057a
Daniel Weck <625201+danielweck@users.noreply.github.com>
2010-04-20 17:11:22 +0000

    Brush <- Color converter for the FlowDocument background

---
+++ tobiapp/Tobi.Common/UI/XAML/ColorToBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Tobi.Common.UI.XAML
+{
+    [ValueConversion(typeof(Color), typeof(Brush))]
+    public class ColorToBrushConverter : ValueConverterMarkupExtensionBase<ColorToBrushConverter>
+    {
+        #region IValueConverter Members
+
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(Brush)) return null;
+            if (!(value is Color)) return null;
+            SolidColorBrush scb = new SolidColorBrush((Color)value);
+            return scb;
+        }
+
+        #endregion
+    }
+}
