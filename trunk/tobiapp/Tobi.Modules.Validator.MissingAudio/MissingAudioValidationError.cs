efc119eac313ed8dae9521b4981f88f7771c4e1b
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2010-04-27 15:43:52 +0000

    fixed missing audio lang resource

--- trunk/tobiapp/Tobi.Modules.Validator.MissingAudio/MissingAudioValidationError.cs
+++ trunk/tobiapp/Tobi.Modules.Validator.MissingAudio/MissingAudioValidationError.cs
@@ -5,16 +5,6 @@ using urakawa.core;
 
 namespace Tobi.Plugin.Validator.MissingAudio
 {
-    public static class Tobi_Plugin_Validator_MissingAudio_Lang
-    {
-        public static string MissingAudioMessage = @"TEMP_DUMMY";
-        public static string MissingAudioSummary = @"TEMP_DUMMY";
-        public static string MissingAudioValidator_Name = @"TEMP_DUMMY";
-        public static string MissingAudioValidator_Description = @"TEMP_DUMMY";
-        public static string MissingAudioMessage2 = @"TEMP_DUMMY";
-        public static string MissingAudio = @"TEMP_DUMMY";
-        public static string ClickToView = @"TEMP_DUMMY";
-    }
     public class MissingAudioValidationError : ValidationItemWithTarget<TreeNode>
     {
         public override string Message
