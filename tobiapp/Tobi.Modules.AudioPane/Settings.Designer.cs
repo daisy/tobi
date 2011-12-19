e2331f471621b464062b2531ecd792287fb6852f
Daniel Weck <625201+danielweck@users.noreply.github.com>
2011-12-19 01:37:27 +0000

    waveform tiling works ok with .NET4, will test .NET3 now.

--- tobiapp/Tobi.Modules.AudioPane/Settings.Designer.cs
+++ tobiapp/Tobi.Modules.AudioPane/Settings.Designer.cs
@@ -97,13 +97,13 @@ namespace Tobi.Plugin.AudioPane {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("20")]
-        public double AudioWaveForm_TextPreRenderThreshold {
+        [global::System.Configuration.DefaultSettingValueAttribute("9999999")]
+        public double AudioWaveForm_TextCacheRenderThreshold {
             get {
-                return ((double)(this["AudioWaveForm_TextPreRenderThreshold"]));
+                return ((double)(this["AudioWaveForm_TextCacheRenderThreshold"]));
             }
             set {
-                this["AudioWaveForm_TextPreRenderThreshold"] = value;
+                this["AudioWaveForm_TextCacheRenderThreshold"] = value;
             }
         }
         
