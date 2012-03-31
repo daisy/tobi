3b1b468eb3c0fd6e2cd0067cda2127f257daa1ab
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2012-03-31 23:11:16 +0000

    - document rendering now with full support for Right To Left (regardless of UI, this is picked-up from the xml:lang and dir="rtl" attribute of the markup tree inside the body/book element) - added UI preference for Right To Left (Waveform display is the only element that remains Left To Right when UI is mirrored on horizontal axis)

--- trunk/tobiapp/Tobi.Common/UI/XAML/WindowStateFullScreenConverter.cs
+++ trunk/tobiapp/Tobi.Common/UI/XAML/WindowStateFullScreenConverter.cs
@@ -4,6 +4,31 @@ using System.Windows.Data;
 
 namespace Tobi.Common.UI.XAML
 {
+    [ValueConversion(typeof(bool), typeof(FlowDirection))]
+    public class WindowFlowDirectionConverter : ValueConverterMarkupExtensionBase<WindowFlowDirectionConverter>
+    {
+        #region IValueConverter Members
+
+        public override object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(FlowDirection))
+                throw new InvalidOperationException("The target must be a WindowState !");
+
+            return ((bool)value ? FlowDirection.RightToLeft : FlowDirection.LeftToRight);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be Boolean !");
+
+            return (FlowDirection)value == FlowDirection.RightToLeft;
+        }
+
+        #endregion
+    }
     [ValueConversion(typeof(bool), typeof(WindowState))]
     public class WindowStateFullScreenConverter : ValueConverterMarkupExtensionBase<WindowStateFullScreenConverter>
     {
