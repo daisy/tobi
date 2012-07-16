0c0fdb8bf2ab5096927f1d622e69ff8d5ab871ea
Daniel Weck <625201+danielweck@users.noreply.github.com>
2012-07-16 12:13:42 +0000

    Tobi now has UI to select MP3 bit rate and stereo at export time.

--- tobiapp/Tobi.Modules.Urakawa/UrakawaSession_Export.cs
+++ tobiapp/Tobi.Modules.Urakawa/UrakawaSession_Export.cs
@@ -113,54 +113,137 @@ namespace Tobi.Plugin.Urakawa
 
 
 
-                    var combo = new ComboBox
+                    var comboSampleRates = new ComboBox
                     {
                         Margin = new Thickness(0, 0, 0, 12)
                     };
 
                     ComboBoxItem item1 = new ComboBoxItem();
                     item1.Content = AudioLib.SampleRate.Hz11025.ToString();
-                    combo.Items.Add(item1);
+                    comboSampleRates.Items.Add(item1);
 
                     ComboBoxItem item2 = new ComboBoxItem();
                     item2.Content = AudioLib.SampleRate.Hz22050.ToString();
-                    combo.Items.Add(item2);
+                    comboSampleRates.Items.Add(item2);
 
                     ComboBoxItem item3 = new ComboBoxItem();
                     item3.Content = AudioLib.SampleRate.Hz44100.ToString();
-                    combo.Items.Add(item3);
+                    comboSampleRates.Items.Add(item3);
 
                     switch (Settings.Default.AudioExportSampleRate)
                     {
                         case AudioLib.SampleRate.Hz11025:
                             {
-                                combo.SelectedItem = item1;
-                                combo.Text = item1.Content.ToString();
+                                comboSampleRates.SelectedItem = item1;
+                                comboSampleRates.Text = item1.Content.ToString();
                                 break;
                             }
                         case AudioLib.SampleRate.Hz22050:
                             {
-                                combo.SelectedItem = item2;
-                                combo.Text = item2.Content.ToString();
+                                comboSampleRates.SelectedItem = item2;
+                                comboSampleRates.Text = item2.Content.ToString();
                                 break;
                             }
                         case AudioLib.SampleRate.Hz44100:
                             {
-                                combo.SelectedItem = item3;
-                                combo.Text = item3.Content.ToString();
+                                comboSampleRates.SelectedItem = item3;
+                                comboSampleRates.Text = item3.Content.ToString();
                                 break;
                             }
                     }
 
-                    var checkBox = new CheckBox
+
+
+                    var comboMp3BitRates = new ComboBox
+                    {
+                        Margin = new Thickness(0, 0, 0, 12)
+                    };
+
+                    ComboBoxItem itemBitRate1 = new ComboBoxItem();
+                    itemBitRate1.Content = AudioLib.Mp3BitRate.kbps_32.ToString();
+                    comboMp3BitRates.Items.Add(itemBitRate1);
+
+                    ComboBoxItem itemBitRate2 = new ComboBoxItem();
+                    itemBitRate2.Content = AudioLib.Mp3BitRate.kbps_48.ToString();
+                    comboMp3BitRates.Items.Add(itemBitRate2);
+
+                    ComboBoxItem itemBitRate3 = new ComboBoxItem();
+                    itemBitRate3.Content = AudioLib.Mp3BitRate.kbps_64.ToString();
+                    comboMp3BitRates.Items.Add(itemBitRate3);
+
+                    ComboBoxItem itemBitRate4 = new ComboBoxItem();
+                    itemBitRate4.Content = AudioLib.Mp3BitRate.kbps_128.ToString();
+                    comboMp3BitRates.Items.Add(itemBitRate4);
+
+                    ComboBoxItem itemBitRate5 = new ComboBoxItem();
+                    itemBitRate5.Content = AudioLib.Mp3BitRate.kbps_196.ToString();
+                    comboMp3BitRates.Items.Add(itemBitRate5);
+
+                    ComboBoxItem itemBitRate6 = new ComboBoxItem();
+                    itemBitRate6.Content = AudioLib.Mp3BitRate.kbps_320.ToString();
+                    comboMp3BitRates.Items.Add(itemBitRate6);
+
+
+                    switch (Settings.Default.AudioExportMp3Bitrate)
+                    {
+                        case AudioLib.Mp3BitRate.kbps_32:
+                            {
+                                comboMp3BitRates.SelectedItem = itemBitRate1;
+                                comboMp3BitRates.Text = itemBitRate1.Content.ToString();
+                                break;
+                            }
+                        case AudioLib.Mp3BitRate.kbps_48:
+                            {
+                                comboMp3BitRates.SelectedItem = itemBitRate2;
+                                comboMp3BitRates.Text = itemBitRate2.Content.ToString();
+                                break;
+                            }
+                        case AudioLib.Mp3BitRate.kbps_64:
+                            {
+                                comboMp3BitRates.SelectedItem = itemBitRate3;
+                                comboMp3BitRates.Text = itemBitRate3.Content.ToString();
+                                break;
+                            }
+                        case AudioLib.Mp3BitRate.kbps_128:
+                            {
+                                comboMp3BitRates.SelectedItem = itemBitRate4;
+                                comboMp3BitRates.Text = itemBitRate4.Content.ToString();
+                                break;
+                            }
+                        case AudioLib.Mp3BitRate.kbps_196:
+                            {
+                                comboMp3BitRates.SelectedItem = itemBitRate5;
+                                comboMp3BitRates.Text = itemBitRate5.Content.ToString();
+                                break;
+                            }
+                        case AudioLib.Mp3BitRate.kbps_320:
+                            {
+                                comboMp3BitRates.SelectedItem = itemBitRate6;
+                                comboMp3BitRates.Text = itemBitRate6.Content.ToString();
+                                break;
+                            }
+                    }
+
+                    var checkBoxEncodeMP3 = new CheckBox
                     {
                         IsThreeState = false,
                         IsChecked = Settings.Default.AudioExportEncodeToMp3,
                         HorizontalAlignment = System.Windows.HorizontalAlignment.Right,
                         VerticalAlignment = VerticalAlignment.Center,
                     };
+                    checkBoxEncodeMP3.Checked += (object sender, RoutedEventArgs e)=>
+                        {
+                            if (checkBoxEncodeMP3.IsChecked.Value)
+                            {
+                                comboMp3BitRates.IsEnabled = true;
+                            }
+                            else
+                            {
+                                comboMp3BitRates.IsEnabled = false;
+                            }
+                        };
 
-                    var label_ = new TextBlock
+                    var labelEncodeMP3 = new TextBlock
                     {
                         Text = Tobi_Plugin_Urakawa_Lang.ExportEncodeMp3,
                         Margin = new Thickness(8, 0, 8, 0),
@@ -171,16 +254,16 @@ namespace Tobi.Plugin.Urakawa
                     };
 
 
-                    var panel__ = new StackPanel
+                    var panelEncodeMP3 = new StackPanel
                     {
                         Orientation = System.Windows.Controls.Orientation.Horizontal,
                         HorizontalAlignment = System.Windows.HorizontalAlignment.Right,
                         VerticalAlignment = VerticalAlignment.Center,
                     };
-                    panel__.Children.Add(label_);
-                    panel__.Children.Add(checkBox);
+                    panelEncodeMP3.Children.Add(labelEncodeMP3);
+                    panelEncodeMP3.Children.Add(checkBoxEncodeMP3);
 
-                    var checkBoxz = new CheckBox
+                    var checkBoxIncludeImageDescriptions = new CheckBox
                     {
                         IsThreeState = false,
                         IsChecked = Settings.Default.ExportIncludeImageDescriptions,
@@ -188,7 +271,7 @@ namespace Tobi.Plugin.Urakawa
                         VerticalAlignment = VerticalAlignment.Center,
                     };
 
-                    var label_z = new TextBlock
+                    var labelIncludeImageDescriptions = new TextBlock
                     {
                         Text = Tobi_Plugin_Urakawa_Lang.ExportIncludeImageDescriptions,
                         Margin = new Thickness(8, 0, 8, 0),
@@ -200,28 +283,60 @@ namespace Tobi.Plugin.Urakawa
 
 
 
-                    var panel_z_ = new StackPanel
+                    var panelIncludeImageDescriptions = new StackPanel
                     {
                         Orientation = System.Windows.Controls.Orientation.Horizontal,
                         HorizontalAlignment = System.Windows.HorizontalAlignment.Right,
                         VerticalAlignment = VerticalAlignment.Center,
                     };
-                    panel_z_.Children.Add(label_z);
-                    panel_z_.Children.Add(checkBoxz);
+                    panelIncludeImageDescriptions.Children.Add(labelIncludeImageDescriptions);
+                    panelIncludeImageDescriptions.Children.Add(checkBoxIncludeImageDescriptions);
 
-                    var panel_ = new StackPanel
+
+                    var checkBoxStereo = new CheckBox
+                    {
+                        IsThreeState = false,
+                        IsChecked = Settings.Default.AudioExportStereo,
+                        HorizontalAlignment = System.Windows.HorizontalAlignment.Right,
+                        VerticalAlignment = VerticalAlignment.Center,
+                    };
+
+                    var labelStereo = new TextBlock
+                    {
+                        Text = Tobi_Plugin_Urakawa_Lang.Stereo,
+                        Margin = new Thickness(8, 0, 8, 0),
+                        HorizontalAlignment = System.Windows.HorizontalAlignment.Right,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Focusable = true,
+                        TextWrapping = TextWrapping.Wrap
+                    };
+
+
+
+                    var panelStereo = new StackPanel
+                    {
+                        Orientation = System.Windows.Controls.Orientation.Horizontal,
+                        HorizontalAlignment = System.Windows.HorizontalAlignment.Right,
+                        VerticalAlignment = VerticalAlignment.Center,
+                    };
+                    panelStereo.Children.Add(labelStereo);
+                    panelStereo.Children.Add(checkBoxStereo);
+
+                    var rootPanel = new StackPanel
                     {
                         Orientation = System.Windows.Controls.Orientation.Vertical,
                         HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch,
                         VerticalAlignment = VerticalAlignment.Center,
                     };
-                    panel_.Children.Add(combo);
-                    panel_.Children.Add(panel__);
-                    panel_.Children.Add(panel_z_);
+                    rootPanel.Children.Add(comboSampleRates);
+                    rootPanel.Children.Add(panelStereo);
+                    rootPanel.Children.Add(panelEncodeMP3);
+                    rootPanel.Children.Add(comboMp3BitRates);
+                    rootPanel.Children.Add(panelIncludeImageDescriptions);
 
                     var windowPopup_ = new PopupModalWindow(m_ShellView,
                                                            UserInterfaceStrings.EscapeMnemonic(Tobi_Plugin_Urakawa_Lang.ExportSettings),
-                                                           panel_,
+                                                           rootPanel,
                                                            PopupModalWindow.DialogButtonsSet.OkCancel,
                                                            PopupModalWindow.DialogButton.Ok,
                                                            false, 300, 180, null, 40, null);
@@ -235,20 +350,47 @@ namespace Tobi.Plugin.Urakawa
                         return;
                     }
 
-                    Settings.Default.AudioExportEncodeToMp3 = checkBox.IsChecked.Value;
-                    Settings.Default.ExportIncludeImageDescriptions = checkBoxz.IsChecked.Value;
+                    Settings.Default.AudioExportStereo = checkBoxStereo.IsChecked.Value;
+                    Settings.Default.AudioExportEncodeToMp3 = checkBoxEncodeMP3.IsChecked.Value;
+                    Settings.Default.ExportIncludeImageDescriptions = checkBoxIncludeImageDescriptions.IsChecked.Value;
 
-                    if (combo.SelectedItem == item1)
+                    if (comboSampleRates.SelectedItem == item1)
                     {
                         Settings.Default.AudioExportSampleRate = SampleRate.Hz11025;
                     }
-                    else if (combo.SelectedItem == item2)
+                    else if (comboSampleRates.SelectedItem == item2)
                     {
                         Settings.Default.AudioExportSampleRate = SampleRate.Hz22050;
                     }
-                    else if (combo.SelectedItem == item3)
+                    else if (comboSampleRates.SelectedItem == item3)
                     {
                         Settings.Default.AudioExportSampleRate = SampleRate.Hz44100;
+                    }
+
+                    
+                    if (comboMp3BitRates.SelectedItem == itemBitRate1)
+                    {
+                        Settings.Default.AudioExportMp3Bitrate = Mp3BitRate.kbps_32;
+                    }
+                    else if (comboMp3BitRates.SelectedItem == itemBitRate2)
+                    {
+                        Settings.Default.AudioExportMp3Bitrate = Mp3BitRate.kbps_48;
+                    }
+                    else if (comboMp3BitRates.SelectedItem == itemBitRate3)
+                    {
+                        Settings.Default.AudioExportMp3Bitrate = Mp3BitRate.kbps_64;
+                    }
+                    else if (comboMp3BitRates.SelectedItem == itemBitRate4)
+                    {
+                        Settings.Default.AudioExportMp3Bitrate = Mp3BitRate.kbps_128;
+                    }
+                    else if (comboMp3BitRates.SelectedItem == itemBitRate5)
+                    {
+                        Settings.Default.AudioExportMp3Bitrate = Mp3BitRate.kbps_196;
+                    }
+                    else if (comboMp3BitRates.SelectedItem == itemBitRate6)
+                    {
+                        Settings.Default.AudioExportMp3Bitrate = Mp3BitRate.kbps_320;
                     }
 
 
