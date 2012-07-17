cdac50a131dd28574339eec632d53a58f493c9fe
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2012-07-17 20:53:12 +0000

    - case-insensitive XML attributes - case-insensitive XML element names in FlowDocument converter - fixes in the FlowDocument generator in order to be more flexible with the HTML mixed inline/block model for certain element names.

--- trunk/tobiapp/Tobi.Modules.NavigationPane/HeadingPanelView.xaml.cs
+++ trunk/tobiapp/Tobi.Modules.NavigationPane/HeadingPanelView.xaml.cs
@@ -131,7 +131,7 @@ namespace Tobi.Plugin.NavigationPane
             HeadingTreeNodeWrapper node = TreeView.SelectedItem as HeadingTreeNodeWrapper;
             if (node == null) return;
 
-            TreeNode treeNode = (node.WrappedTreeNode_LevelHeading ?? node.WrappedTreeNode_Level.GetFirstDescendantWithText());
+            TreeNode treeNode = (node.WrappedTreeNode_LevelHeading ?? (node.WrappedTreeNode_Level.GetFirstDescendantWithText() ?? node.WrappedTreeNode_Level));
             if (treeNode == null) return;
 
             //if (m_ignoreHeadingSelected)
