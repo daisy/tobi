aea2dbb2da354ca27b0373697f9b963028a34f7c
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2012-11-06 08:30:37 +0000

    Workaround for WPF 4 bug whereby system font families may raise an exception when accessed by name with some multi-culture fonts.

---
+++ trunk/tobiapp/Tobi.Common/UI/FontsFix.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace Tobi.Common.UI
+{
+    public static class FontsFix
+    {
+        private static ReadOnlyCollection<FontFamily> m_SystemFontFamilies;
+        public static ReadOnlyCollection<FontFamily> SystemFontFamilies
+        {
+            get
+            {
+                if (m_SystemFontFamilies == null)
+                {
+                    var sysFontFamilies = new List<FontFamily>();
+                    foreach (FontFamily sysFontFamily in Fonts.SystemFontFamilies)
+                    {
+                        try
+                        {
+                            var familyNames = sysFontFamily.FamilyNames; // exception possible
+
+                            foreach (KeyValuePair<XmlLanguage, string> familyName in familyNames)
+                            {
+                                Console.WriteLine(familyName.Key + @" => " + familyName.Value);
+                            }
+
+                            sysFontFamilies.Add(sysFontFamily);
+                        }
+                        catch (ArgumentException)
+                        {
+                            // WPF 4 bug => possible exception with some font names (due to multiple CultureInfo)
+                        }
+                    }
+                    m_SystemFontFamilies = sysFontFamilies.AsReadOnly();
+                }
+
+                return m_SystemFontFamilies;
+            }
+        }
+    }
+}
