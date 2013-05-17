e2b5de693d29039314dedce7c0687153cf047aa0
Daniel Weck <625201+danielweck@users.noreply.github.com>
2013-05-17 13:36:32 +0000

    added export feature: checkbox (true by default) to generate SMIL follow-up virtual note / annotation immediately after noteref / annoref.

--- tobiapp/Tobi.Modules.Urakawa/Settings.Designer.cs
+++ tobiapp/Tobi.Modules.Urakawa/Settings.Designer.cs
@@ -86,6 +86,18 @@ namespace Tobi.Plugin.Urakawa {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool ExportGenerateSmilNotes {
+            get {
+                return ((bool)(this["ExportGenerateSmilNotes"]));
+            }
+            set {
+                this["ExportGenerateSmilNotes"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool ExportIncludeImageDescriptions {
             get {
                 return ((bool)(this["ExportIncludeImageDescriptions"]));
