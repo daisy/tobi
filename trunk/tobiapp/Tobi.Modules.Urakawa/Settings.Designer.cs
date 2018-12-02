6bd9fe4e944629ecdfd133b05252677d1e17565c
daniel-weck <daniel-weck@0210928e-6ab4-4709-a5c3-da57979d938b>
2018-12-02 08:51:24 +0000

    renamed AudioExportFileCreateLevelNames to AudioExportFileSplitLevelNames (usage of the term "split"), and added to export dialog a combo box choice enumeration, including DEFAULT (which has historically been: level*, section), CURRENT (the active user setting), and CUSTOMs (predefined: level1 - level2 - level3).

--- trunk/tobiapp/Tobi.Modules.Urakawa/Settings.Designer.cs
+++ trunk/tobiapp/Tobi.Modules.Urakawa/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Tobi.Plugin.Urakawa {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -86,12 +86,12 @@ namespace Tobi.Plugin.Urakawa {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string AudioExportFileCreateLevelNames {
+        public string AudioExportFileSplitLevelNames {
             get {
-                return ((string)(this["AudioExportFileCreateLevelNames"]));
+                return ((string)(this["AudioExportFileSplitLevelNames"]));
             }
             set {
-                this["AudioExportFileCreateLevelNames"] = value;
+                this["AudioExportFileSplitLevelNames"] = value;
             }
         }
         
